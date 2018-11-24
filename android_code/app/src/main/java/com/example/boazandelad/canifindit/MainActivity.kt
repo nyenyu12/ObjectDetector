fc213d6083f707344c6e6b96ac7f414c056b3c0d
Elad Roichman <roichman1@gmail.com>
2018-11-24 18:27:14 +0200

    Initial commit

---
+++ android_code/app/src/main/java/com/example/boazandelad/canifindit/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.boazandelad.canifindit
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build.VERSION.SDK_INT
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.boazandelad.canifindit.MainActivity.Companion.SERVERPORT
+import com.example.boazandelad.canifindit.MainActivity.Companion.SERVER_IP
+import java.lang.Exception
+import khttp.post
+import android.os.StrictMode
+import khttp.extensions.fileLike
+import kotlinx.coroutines.*
+import java.io.*
+import java.nio.charset.Charset
+import java.util.*
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.EditText
+import khttp.responses.Response
+import java.net.*
+
+fun getRealPathFromURI(uri: Uri, context: Context): String {
+    val cursor = context.contentResolver.query(uri, null, null, null, null)
+    cursor.moveToFirst()
+    val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+    return cursor.getString(idx)
+}
+
+class MainActivity : AppCompatActivity() {
+
+    private var imageView:ImageView? = null
+    companion object {
+        const val PICK_IMAGE:Int = 100
+        val SERVERPORT = 5000
+        val SERVER_IP = "10.0.0.1"
+        val REQUEST_MEMORY_ACCESS = 101
+        var path: String? = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)*/
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            findViewById<TextView>(R.id.image_desc).text= "Premission not granted immidietly"
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_MEMORY_ACCESS)
+        }
+
+        imageView = findViewById(R.id.pic_view)
+
+    }
+
+    fun openGallery(view: View){
+        var gallery = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, PICK_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE){
+            var imageUri = data?.data
+            imageView?.setImageURI(imageUri)
+            if (imageUri!=null) {
+                path = getRealPathFromURI(imageUri, baseContext)
+            }
+            findViewById<TextView>(R.id.image_desc).text= "Image not processed yet"
+        }
+    }
+
+
+    fun sendImage (view: View) = try {
+        var bmp = (imageView?.drawable as BitmapDrawable).bitmap
+        var bos = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 0, bos)
+        var bArrray = bos.toByteArray()
+        var response: Response? = null
+        val Job = GlobalScope.launch {
+            try {
+                val url:String = "http://${findViewById<EditText>(R.id.IPText).text}:${findViewById<EditText>(R.id.PortText).text}"
+                findViewById<TextView>(R.id.image_desc).text = "Connecting to " + url;
+                response = post(url, files = listOf(File(path).fileLike(name = "Image.jpg")))
+                findViewById<TextView>(R.id.image_desc).text = "Image contains: ${response?.text}"
+            }
+            catch (e: Exception){
+                findViewById<TextView>(R.id.image_desc).text = "Connection failed - please check fields are valid"
+            }
+        }
+
+    } catch (e: UnknownHostException){
+        findViewById<TextView>(R.id.image_desc).text = "Unknown host :("
+        e.printStackTrace()
+    } catch (e: IOException) {
+        findViewById<TextView>(R.id.image_desc).text = "IO exceptiion :("
+        e.printStackTrace()
+    } catch (e: Exception){
+        findViewById<TextView>(R.id.image_desc).text = "Other exception :("
+        e.printStackTrace()
+    }
+}
---
+++ python_code/classifier.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+import torch.nn.functional as F
+import pretrainedmodels as pre
+import pretrainedmodels.utils as utils
+
+#print (pre.model_names)
+#model=pre.pnasnet5large(1000)
+#print (model)
+#torch.save(model, "model.txt")
+
+classes=np.array(open('imagenet_classes.txt','r').read().split('\n'))
+
+device=("cuda:0" if torch.cuda.is_available() else "cpu")
+print ("Using "+device)
+
+model=torch.load('model.txt').to(device)
+model.eval()
+
+def prep_image(img,model,device):
+    # Load and Transform one input image
+    tf_img = utils.TransformImage(model)
+    input_data = tf_img(img)
+    input_data = input_data.unsqueeze(0)
+    return input_data.to(device)
+
+def classify(img,model):
+    output=model.forward(img)
+    return classes[torch.argmax(output)]
+
+def classify_in_memory(path_img,model=model,device=device):
+    load_img=utils.LoadImage()
+    input_data=prep_image(load_img(path_img),model,device)
+    return classify(input_data,model)
+
+if __name__=='__main__':
+    print (classify_in_memory('./test/kora2.jpg'))
